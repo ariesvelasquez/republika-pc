bfad04e3ef4457e631c0a6ce778341a196d031de
Aries Velasquez <velasquezaries11@gmail.com>
2020-01-05 03:14:06 +0800

    Integrated crashlytics for release prod apps

--- app/src/main/java/ariesvelasquez/com/republikapc/RepublikaPC.java
+++ app/src/main/java/ariesvelasquez/com/republikapc/RepublikaPC.java
@@ -1,15 +1,10 @@
 package ariesvelasquez.com.republikapc;
 
 import android.app.Application;
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.security.ProviderInstaller;
+import android.util.Log;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLEngine;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 
 public class RepublikaPC extends Application {
 
@@ -24,6 +19,9 @@ public class RepublikaPC extends Application {
         // Setup Timber Logger
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+        } else {
+            Fabric.with(this, new Crashlytics());
+            Timber.plant(new CrashReportingTree());
         }
 
 //        try {
@@ -46,6 +44,29 @@ public class RepublikaPC extends Application {
 //                | NoSuchAlgorithmException | KeyManagementException e) {
 //            e.printStackTrace();
 //        }
+    }
+
+    private class CrashReportingTree extends Timber.Tree {
+        private static final String CRASHLYTICS_KEY_PRIORITY = "priority";
+        private static final String CRASHLYTICS_KEY_TAG = "tag";
+        private static final String CRASHLYTICS_KEY_MESSAGE = "message";
+
+        @Override
+        protected void log(int priority, String tag, String message, Throwable throwable) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            Throwable t = throwable != null
+                    ? throwable
+                    : new Exception(message);
+
+            // Crashlytics
+            Crashlytics.setInt(CRASHLYTICS_KEY_PRIORITY, priority);
+            Crashlytics.setString(CRASHLYTICS_KEY_TAG, tag);
+            Crashlytics.setString(CRASHLYTICS_KEY_MESSAGE, message);
+            Crashlytics.logException(t);
+        }
     }
 
     public static GlobalFlags getGlobalFlags() {
